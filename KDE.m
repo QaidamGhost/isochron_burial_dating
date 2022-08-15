4e89f83af0cd8004a419c82e5b73d3e8ec10a4a3
Yizhou Yang <55291650+QaidamGhost@users.noreply.github.com>
2022-08-15 12:06:47 +0800

    Update KDE.m
    Forced mean value can be input as an argument for error calculation using KDE.

--- KDE.m
+++ KDE.m
@@ -1,15 +1,21 @@
-function [most_prob,upper_sigma,lower_sigma] = KDE(cache)
+function [most_prob,upper_sigma,lower_sigma] = KDE(cache,cache_mean)
 
 %% Find the most probable value and 1 sigma absolute error for the observed simulations of an random variable after establishing the probability density function by a "nonparametric estimation" approach knows as kernel density estimation (KDE).
+% If the mean value of cache is input as an argument, it will replace the most probable value estimated by the KDE and final 1 sigma error will surround this value. 
 
 %% Arguments: 
 % cache: the observed simulations of an random variable to be evaluated
 % (1×n vector)
+% cache_mean: input most probable value (scalar; optional) 
 
 %% Output:
 % most_prob: the most probable value of the variable (scalar)
 % upper_sigma: the upper 1 sigma absolute error of the variable (scalar)
 % lower_sigma: the lower 1 sigma absolute error of the variable (scalar)
+
+    if nargin==1 
+        cache_mean=NaN; 
+    end
 
     step=1E-4;
     min_cache=min(cache);
@@ -17,8 +23,13 @@ function [most_prob,upper_sigma,lower_sigma] = KDE(cache)
     bounded_cache=min_cache:step:max_cache;
     % search the most probable value from the simulations
     f=ksdensity(cache,bounded_cache);
-    [~,index]=max(f);
-    most_prob=bounded_cache(index);
+    if isnan(cache_mean)
+        [~,index]=max(f);
+        most_prob=bounded_cache(index);
+    elseif ~isnan(cache_mean)
+        most_prob=cache_mean; 
+        index=fix((most_prob-min_cache)/step)+1; 
+    end
     % fake-normalization with the given step (F to 1)
     nf=f/sum(f*step);
     most_prob_cdp=sum(nf(1:index))*step;
