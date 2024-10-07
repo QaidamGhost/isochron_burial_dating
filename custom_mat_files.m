4d8f38a87a1c138a7a1336c6dee90ce675070220
QaidamGhost <55291650+QaidamGhost@users.noreply.github.com>
2024-10-08 01:04:55 +0800

    Update custom_mat_files.m
    Replace term "maximum" with "upper limit".

--- custom_mat_files.m
+++ custom_mat_files.m
@@ -1,6 +1,6 @@
 function custom_mat_files(erosion_rate,sigma_erosion_rate,expo_age,sigma_expo_age)
 
-%% Generate "e.mat" and "expo_age.mat" if you want to calculate a maximum isochron burial age and already know the value and absolute 1 sigma error of erosion rate and exposure age.
+%% Generate "e.mat" and "expo_age.mat" if you want to calculate the upper limit of the isochron burial age and already know the value and absolute 1 sigma error of erosion rate and exposure age.
 % Note that the input exposure age and erosion rate should be Gaussian
 % distribution, or you should use Hidy's modified scripts to generate the
 % "expo_age.mat" file.
